b0baa8a99a1371cfbffac24a4e8bae6850d39d01
Simon Delcourt <delcourt.simon@gmail.com>
2013-07-09 14:29:26 +0000

    filter titles for architect search

--- src/imio/urban/dataimport/AIHM/mappers.py
+++ src/imio/urban/dataimport/AIHM/mappers.py
@@ -128,7 +128,7 @@ class ArchitectMapper(PostCreationMapper):
         fullname = cleanAndSplitWord(archi_name)
         if not fullname:
             return []
-        noisy_words = ['monsieur', 'madame', 'architecte', '&', ',', '.', 'or']
+        noisy_words = ['monsieur', 'madame', 'architecte', '&', ',', '.', 'or', 'mr', 'mme']
         name_keywords = [word.lower() for word in fullname if word.lower() not in noisy_words]
         architects = self.catalog(portal_type='Architect', Title=name_keywords)
         if len(architects) == 1:
