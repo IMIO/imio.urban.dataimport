ca5b724e896a93294bc6d512b663e377229637ef
Simon Delcourt <delcourt.simon@gmail.com>
2015-12-01 12:42:56 +0000

    remove undo/redo action panel

--- src/imio/urban/dataimport/browser/controlpanel.py
+++ src/imio/urban/dataimport/browser/controlpanel.py
@@ -6,8 +6,6 @@ from Products.Five import BrowserView
 from Products.Five.browser.pagetemplatefile import ViewPageTemplateFile
 
 from imio.urban.dataimport.browser.import_panel import ImporterSettings
-from imio.urban.dataimport.browser.undo_panel import RedoImportPanel
-from imio.urban.dataimport.browser.undo_panel import UndoImportPanel
 
 
 class ImporterControlPanel(BrowserView):
@@ -15,14 +13,10 @@ class ImporterControlPanel(BrowserView):
     """
 
     import_form = ImporterSettings
-    undo_form = UndoImportPanel
-    redo_form = RedoImportPanel
 
     def __init__(self, context, request):
         super(ImporterControlPanel, self).__init__(context, request)
         self.import_form_instance = self.import_form(aq_inner(context), request)
-        self.undo_form_instance = self.undo_form(aq_inner(context), request)
-        self.redo_form_instance = self.redo_form(aq_inner(context), request)
 
     def __call__(self):
         self.update()
@@ -30,8 +24,6 @@ class ImporterControlPanel(BrowserView):
 
     def update(self):
         self.import_form_instance()
-        self.undo_form_instance()
-        self.redo_form_instance()
 
     def render(self):
         controlpanel_template = ViewPageTemplateFile('controlpanel.pt')
