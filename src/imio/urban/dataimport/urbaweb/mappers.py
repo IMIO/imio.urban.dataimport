2f7742e20c482090d06e14119cc383056882a380
Julien Jaumotte <julien.jaumotte@imio.be>
2017-06-22 12:04:58 +0000

    Fix old method param

--- src/imio/urban/dataimport/urbaweb/mappers.py
+++ src/imio/urban/dataimport/urbaweb/mappers.py
@@ -518,7 +518,7 @@ class ParcelFactory(BaseFactory):
         #check if we can find a parcel in the db cadastre with these infos
         found = searchview.search_parcels_custom(**parcel_args)
         if not found:
-            found = searchview.search_parcels_custom(browseoldparcels=True, **parcel_args)
+            found = searchview.search_parcels_custom(old=True, **parcel_args)
 
         if len(found) == 1 and parcel.has_same_attribute_values(found[0]):
             parcel_args['divisionCode'] = parcel_args['division']
