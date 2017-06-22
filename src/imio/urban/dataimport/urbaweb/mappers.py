c49135ee3e519ee044b2cf8019c5dc8aa1d06368
Julien Jaumotte <julien.jaumotte@imio.be>
2017-06-22 11:50:47 +0000

    Change default parcel searching method with new 2.2 product method

--- src/imio/urban/dataimport/urbaweb/mappers.py
+++ src/imio/urban/dataimport/urbaweb/mappers.py
@@ -516,9 +516,9 @@ class ParcelFactory(BaseFactory):
         for k, v in parcel_args.iteritems():
             searchview.context.REQUEST[k] = v
         #check if we can find a parcel in the db cadastre with these infos
-        found = searchview.findParcel(**parcel_args)
+        found = searchview.search_parcels_custom(**parcel_args)
         if not found:
-            found = searchview.findParcel(browseoldparcels=True, **parcel_args)
+            found = searchview.search_parcels_custom(browseoldparcels=True, **parcel_args)
 
         if len(found) == 1 and parcel.has_same_attribute_values(found[0]):
             parcel_args['divisionCode'] = parcel_args['division']
